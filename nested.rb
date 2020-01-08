24042410a1c7003ad0b04d2effbd9f9bbcd47b87
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-08 00:48:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,10 +68,6 @@ def adding_matz
 
 	programmer_hash = 
  		{
- 		    :yukihiro_matsumoto => {
-          :known_for => "Ruby",
-          :languages => ["LISP", "C"]
-         },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -107,7 +103,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash
      
 end
 
